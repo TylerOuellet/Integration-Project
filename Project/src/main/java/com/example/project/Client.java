85790745a8b21a704cdbfad6c6969994c0d71772
bujoldzack <zbujold_26@hotmail.ca>
2023-11-24 14:37:16 -0500

    javadoc comments

--- Project/src/main/java/com/example/project/Client.java
+++ Project/src/main/java/com/example/project/Client.java
@@ -2,12 +2,19 @@ package com.example.project;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a client that extends the user class.
+ * Additional information like the client ID, the email, and the signup time.
+ */
 public class Client extends User {
 
+    /**Hold the client ID*/
     private String aClientId;
 
+    /**Hold the email*/
     private String aEmail;
 
+    /**Hold the time*/
     private LocalDateTime aSignUP;
 
 
--- Project/src/main/java/com/example/project/User.java
+++ Project/src/main/java/com/example/project/User.java
@@ -1,10 +1,16 @@
 package com.example.project;
 
+/**
+ * Represents a user with a username, password, and if the user is a manager or not.
+ */
 public class User {
-    private String aPassword;
 
+    /** Hold the user's username.*/
     private String aUsername;
 
-    private Boolean aIsManager;
+     /** Hold the user's password.*/
+    private String aPassword;
 
+    /** Hold the information of which user is a manager.*/
+    private Boolean aIsManager;
 }
