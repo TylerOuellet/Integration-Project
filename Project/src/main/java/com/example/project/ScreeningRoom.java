e6e1eb1fa3ee5ecc70083db5e357dd78f1dff7c6
bujoldzack <zbujold_26@hotmail.ca>
2023-11-27 09:24:45 -0500

    java docs for Screening Room, Showtime and Ticket.

--- Project/src/main/java/com/example/project/ScreeningRoom.java
+++ Project/src/main/java/com/example/project/ScreeningRoom.java
@@ -1,19 +1,41 @@
 package com.example.project;
 
+
+/**
+ * Represents the screening room in the movie theater.
+ * Used to store information about the screening room.
+ */
 public class ScreeningRoom implements ManagementCollection {
 
+    /** The remaining seats in the screening room. */
     private int aRemainingSeats;
+
+    /** The room ID of the screening room. */
     private String aRoomID;
 
+    /**
+     * Constructor
+     * @param pRemainingSeats The remaining seats.
+     * @param pRoomID The room ID.
+     */
     public ScreeningRoom(int pRemainingSeats, String pRoomID) {
         setRemainingSeats(pRemainingSeats);
         setRoomID(pRoomID);
     }
 
+    /**
+     * Getter of the Remaining seats.
+     * @return the remaining seats.
+     */
     public int getRemainingSeats() {
         return aRemainingSeats;
     }
 
+    /**
+     * Setter of the remaining seats.
+     * @param pRemainingSeats the remaining seats.
+     * @throws IllegalArgumentException if the remaining seats is a negative number.
+     */
     public void setRemainingSeats(int pRemainingSeats) {
         if (pRemainingSeats >= 0) {
             this.aRemainingSeats = pRemainingSeats;
@@ -22,10 +44,19 @@ public class ScreeningRoom implements ManagementCollection {
         }
     }
 
+    /**
+     * Getter of the room ID.
+     * @return the room ID.
+     */
     public String getRoomID() {
         return aRoomID;
     }
 
+    /**
+     * Setter of the room ID.
+     * @param pRoomID the room ID.
+     * @throws IllegalArgumentException if the room ID is null or empty.
+     */
     public void setRoomID(String pRoomID) {
         if (pRoomID != null && !pRoomID.isEmpty()) {
             this.aRoomID = pRoomID;
--- Project/src/main/java/com/example/project/Ticket.java
+++ Project/src/main/java/com/example/project/Ticket.java
@@ -2,23 +2,46 @@ package com.example.project;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents the Ticket of the movie theater.
+ * Used to store the information about the ticket.
+ */
 public class Ticket {
+
+    /** The ticket ID. */
     private String aTicketID;
 
+    /** The date and time of the purchase. */
     private LocalDateTime aPurchaseDateTime;
 
+    /** The showtime of the ticket. */
     private Showtime aForShowtime;
 
+    /**
+     * Constructor.
+     * @param pTicketID The ticket ID.
+     * @param pPurchaseDateTime The date and time of the purchase.
+     * @param pForShowtime The showtime of the ticket.
+     */
     public Ticket(String pTicketID, LocalDateTime pPurchaseDateTime, Showtime pForShowtime) {
         setTicketID(pTicketID);
         setPurchaseDateTime(pPurchaseDateTime);
         setShowtime(pForShowtime);
     }
 
+    /**
+     * Getter of the ticket ID.
+     * @return the ticket ID.
+     */
     public String getTicketID() {
         return aTicketID;
     }
 
+    /**
+     * Setter of the ticket ID.
+     * @param pTicketID the ticket ID.
+     * @throws IllegalArgumentException if the ticket ID is null or empty.
+     */
     public void setTicketID(String pTicketID) {
         if (pTicketID != null && !pTicketID.isEmpty()) {
             this.aTicketID = pTicketID;
@@ -27,10 +50,19 @@ public class Ticket {
         }
     }
 
+    /**
+     * Getter of the purchase date and time.
+     * @return the purchase date and time.
+     */
     public LocalDateTime getPurchaseDateTime() {
         return aPurchaseDateTime;
     }
 
+    /**
+     * The setter of the date and time of the purchase.
+     * @param pPurchaseDateTime The purchase date and time.
+     * @throws IllegalArgumentException if the purchase date and time is null.
+     */
     public void setPurchaseDateTime(LocalDateTime pPurchaseDateTime) {
         if (pPurchaseDateTime != null) {
             this.aPurchaseDateTime = pPurchaseDateTime;
@@ -39,10 +71,19 @@ public class Ticket {
         }
     }
 
+    /**
+     * Getter of the showtime for the ticket.
+     * @return the showtime of the ticket.
+     */
     public Showtime getForShowtime() {
         return aForShowtime;
     }
 
+    /**
+     * Setter of the showtime for the ticket.
+     * @param pForShowtime showtime for the ticket.
+     * @throws IllegalArgumentException of the showtime is null.
+     */
     public void setShowtime(Showtime pForShowtime) {
         if (pForShowtime != null) {
             this.aForShowtime = pForShowtime;
