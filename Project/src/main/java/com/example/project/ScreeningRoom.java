43024f92426e754dd61adae15ae08f2e2b60ae84
bujoldzack <zbujold_26@hotmail.ca>
2023-11-27 08:38:56 -0500

    input validation: Screening Room, Showtime, Ticket.

--- Project/src/main/java/com/example/project/ScreeningRoom.java
+++ Project/src/main/java/com/example/project/ScreeningRoom.java
@@ -3,7 +3,34 @@ package com.example.project;
 public class ScreeningRoom implements ManagementCollection {
 
     private int aRemainingSeats;
-
     private String aRoomID;
 
+    public ScreeningRoom(int pRemainingSeats, String pRoomID) {
+        setRemainingSeats(pRemainingSeats);
+        setRoomID(pRoomID);
+    }
+
+    public int getRemainingSeats() {
+        return aRemainingSeats;
+    }
+
+    public void setRemainingSeats(int pRemainingSeats) {
+        if (pRemainingSeats >= 0) {
+            this.aRemainingSeats = pRemainingSeats;
+        } else {
+            throw new IllegalArgumentException("The remaining seats cannot be negative.");
+        }
+    }
+
+    public String getRoomID() {
+        return aRoomID;
+    }
+
+    public void setRoomID(String pRoomID) {
+        if (pRoomID != null && !pRoomID.isEmpty()) {
+            this.aRoomID = pRoomID;
+        } else {
+            throw new IllegalArgumentException("The Room ID cannot be null or empty.");
+        }
+    }
 }
--- Project/src/main/java/com/example/project/Ticket.java
+++ Project/src/main/java/com/example/project/Ticket.java
@@ -8,4 +8,46 @@ public class Ticket {
     private LocalDateTime aPurchaseDateTime;
 
     private Showtime aForShowtime;
+
+    public Ticket(String pTicketID, LocalDateTime pPurchaseDateTime, Showtime pForShowtime) {
+        setTicketID(pTicketID);
+        setPurchaseDateTime(pPurchaseDateTime);
+        setShowtime(pForShowtime);
+    }
+
+    public String getTicketID() {
+        return aTicketID;
+    }
+
+    public void setTicketID(String pTicketID) {
+        if (pTicketID != null && !pTicketID.isEmpty()) {
+            this.aTicketID = pTicketID;
+        } else {
+            throw new IllegalArgumentException("Ticket ID cannot be null or empty.");
+        }
+    }
+
+    public LocalDateTime getPurchaseDateTime() {
+        return aPurchaseDateTime;
+    }
+
+    public void setPurchaseDateTime(LocalDateTime pPurchaseDateTime) {
+        if (pPurchaseDateTime != null) {
+            this.aPurchaseDateTime = pPurchaseDateTime;
+        } else {
+            throw new IllegalArgumentException("Purchase DateTime cannot be null.");
+        }
+    }
+
+    public Showtime getForShowtime() {
+        return aForShowtime;
+    }
+
+    public void setShowtime(Showtime pForShowtime) {
+        if (pForShowtime != null) {
+            this.aForShowtime = pForShowtime;
+        } else {
+            throw new IllegalArgumentException("Showtime cannot be null.");
+        }
+    }
 }
