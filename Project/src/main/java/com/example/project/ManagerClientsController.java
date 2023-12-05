b6fef6241747af06c5d9a34039c373658b43fe48
bujoldzack <zbujold_26@hotmail.ca>
2023-12-05 11:46:47 -0500

    clients name into the list view when controller initialize.

--- Project/src/main/java/com/example/project/ManagerClientsController.java
+++ Project/src/main/java/com/example/project/ManagerClientsController.java
@@ -1,4 +1,29 @@
 package com.example.project;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManagerClientsController {
+
+    @FXML
+    ListView<String> clientsListView;
+
+    @FXML
+    private void initialize() {
+        UserService userService = new UserService(UserDataStorage.loadUsers());
+
+        List<ConcreteUser> users = userService.getUsers();
+
+        List<String> clientNames = new ArrayList<>();
+        for (ConcreteUser user : users) {
+            if (!user.getManagerStatus()) {
+                clientNames.add(user.getName());
+            }
+        }
+
+        clientsListView.getItems().addAll(clientNames);
+    }
 }
